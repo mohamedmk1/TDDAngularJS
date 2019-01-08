685b0656829a12c093bd4aaf8f8ede16bc442c7f
mohamedmk1 <lmabrouk.mohamed@gmail.com>
2019-01-08 23:47:23 +0100

    first commit

---
+++ src/app/app.js
@@ -0,0 +1,28 @@
+import angular from 'angular';
+import 'bootstrap';
+
+import '../style/app.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+let app = () => {
+  return {
+    template: require('./app.html'),
+    controller: 'AppCtrl',
+    controllerAs: 'vm'
+  }
+};
+
+class AppCtrl {
+  constructor() {
+    var vm = this;
+    vm.url = 'https://github.com/preboot/angular-webpack';
+  }
+}
+
+const MODULE_NAME = 'app';
+
+angular.module(MODULE_NAME, [])
+  .directive('app', app)
+  .controller('AppCtrl', AppCtrl);
+
+export default MODULE_NAME;
